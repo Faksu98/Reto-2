7380ee38b8959f718d45d1faa620fd06e3a3eb73
faksu <dk>
2022-06-02 15:30:54 -0300

    nreto

--- Reto.js
+++ Reto.js
@@ -6,15 +6,22 @@ let f = "por def";
 
 Esto deberia crear las casillas
 */
-for (let i = 0 ; i <= CASILLAS_MAX ; i++){
+for (let i = 1 ; i <= CASILLAS_MAX ; i++){
     document.querySelector("#casillas").innerHTML+=`
     <input id="Casi${i}" type="checkbox">Casilla ${i} <br>
     `;
 }
 function alterar(){
+    /*
+        if (f != "por def"){
+       document.querySelector(`#Op${f}`).innerHTML= `
+       <input id="Casi${f}" type="checkbox">Casilla ${f} <br>
+       `;
+    }
+    */
     f = document.querySelector("#alterar_casillas").value;
-    for ( f ; f <= CASILLAS_MAX ; f++){
-        document.querySelector(`#Casi${f}`).innerHTML+=`
+    for (let n = 1 ; n <= f ; n++){
+        document.querySelector(`#Casi${n}`).innerHTML+=`
         <input type="checkbox" checked="checked"/>
         `;
     }
