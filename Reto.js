b20bb24cbc0e87674b0d9e676e8ce3657a9bf201
faksu <dk>
2022-06-02 15:05:44 -0300

    avancereto

--- Reto.js
+++ Reto.js
@@ -1,10 +1,26 @@
 const CASILLAS_MAX = 20;
 
 let f = "por def";
+let casillasDesmarcadas = "por def";
 
-for (let i = 1 ; i = CASILLAS_MAX ; i++){
+
+/*
+
+Esto deberia crear las casillas
+
+for (let i = 0 ; i = CASILLAS_MAX ; i++){
     document.querySelector("#casillas").innerHTML+=`
-    <input id="Casi${i}"/>Casilla ${i}
-    <br/>
+    <input id="Casi${i}" type="checkbox">Casilla ${i} <input/>
     `;
+}
+*/
+
+
+function alterar(){
+    f = document.querySelector("#alterar_casillas").value;
+    for ( f ; f <= CASILLAS_MAX ; f++){
+        document.querySelector("#Casi").innerHTML+=`
+        <input id="Casi" type="checkbox" checked="checked"/>
+        `;
+    }
 }
