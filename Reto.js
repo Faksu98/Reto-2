a849adecb9caee32e873cb19e094424ce6961ff0
faksu <dk>
2022-06-03 13:27:20 -0300

    quincenalterminado

--- Reto.js
+++ Reto.js
@@ -1,28 +1,15 @@
-const CASILLAS_MAX = 20;
-
-let f = "por def";
-
-/*
-
-Esto deberia crear las casillas
-*/
-for (let i = 1 ; i <= CASILLAS_MAX ; i++){
-    document.querySelector("#casillas").innerHTML+=`
-    <input id="Casi${i}" type="checkbox">Casilla ${i} <br>
-    `;
-}
-function alterar(){
-    /*
-        if (f != "por def"){
-       document.querySelector(`#Op${f}`).innerHTML= `
-       <input id="Casi${f}" type="checkbox">Casilla ${f} <br>
-       `;
-    }
-    */
-    f = document.querySelector("#alterar_casillas").value;
-    for (let n = 1 ; n <= f ; n++){
-        document.querySelector(`#Casi${n}`).innerHTML+=`
-        <input type="checkbox" checked="checked"/>
-        `;
-    }
+const CASILLAS_MAX = 20; 
+ 
+let f = "por def"; 
+ 
+for (let i = 1 ; i <= CASILLAS_MAX ; i++){ 
+    document.querySelector("#casillas").innerHTML+=` 
+    <input id="casi${i}" type="checkbox">Casilla ${i} <br> 
+    `; 
+} 
+function alterar(){ 
+    f = Number(document.querySelector("#alterar_casillas").value); 
+    for (let n = 1 ; n <= CASILLAS_MAX ; n = n + f){ 
+        document.querySelector(`#casi${n}`).checked = true; 
+    } 
 }
